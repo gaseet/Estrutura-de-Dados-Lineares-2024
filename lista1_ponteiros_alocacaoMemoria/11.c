c206bbb0d682884a9e2b54d3a25c4f8f5830c6f2
gaseet <45747276+gaseet@users.noreply.github.com>
2024-03-03 18:17:03 -0300

    Update 11.c

--- lista1_ponteiros_alocacaoMemoria/11.c
+++ lista1_ponteiros_alocacaoMemoria/11.c
@@ -1,8 +1,3 @@
-//Um ponteiro pode ser usado para dizer a uma função onde ela deve depositar o
-//resultado de seus cálculos. Escreva uma função xxx que converta minutos em horas-eminutos.
-//A função recebe um inteiro mnts e os endereços de duas variáveis inteiras,
-//digamos h e m, e atribui valores a essas variáveis de modo que m seja menor que 60 e que
-//60*h + m seja igual a mnts. Escreva também uma função main que use a função xxx. 
 #include <stdio.h>
 
 void minutosParaHorasMinutos(int mnts, int *h, int *m) {
